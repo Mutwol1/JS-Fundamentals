445c5232e4f10d67c35cd05d1b48b1cf74f379a9
Godfrey Mutwol <mutwolgodfrey@gmail.com>
2025-07-12 01:41:34 +0300

    JS-Fundamentals

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,17 @@
 
 #!/usr/bin/node
 
-if (typeof process.argv[2] === 'undefined') {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
+function factorial(n) {
+  const num = parseInt(n);
+  if (isNaN(num)) {
+    return 1;
+  }
+  if (num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 }
+
+const arg = process.argv[2];
+const result = factorial(arg);
+console.log(result);
